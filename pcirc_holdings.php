1b7924b25a2516b0a765c13a04d05229825a0076
Ken Irwin <kirwin@wittenberg.edu>
2019-05-08 15:52:58 -0400

    pcirc_holdings work ok

--- pcirc_holdings.php
+++ pcirc_holdings.php
@@ -6,36 +6,44 @@
 <h2>Add Local Holdings Information</h2>
 <p><a href="index.php">Return to View Top InnReach Requests</a></p>
 <?php
-   /*
+
   error_reporting(E_ALL);
   ini_set("display_errors", true);
-   */
+
 include ("config.php");
-include ("mysql_connect.php");
+include ("pdo_connect.php");
 
 if ($_REQUEST['submit_button']) {
   foreach ($_REQUEST['holdings'] as $call=>$status) {
     if (strlen($status) > 0) { //if new info
       $realcall = $_REQUEST['calls'][$call];
-      $q = "UPDATE `innreach_titles_by_call` SET `have`='$status' WHERE `call` = '$realcall'";
-      if (mysql_query($q)) { print "<span class=good>DONE: $q</span><br>\n"; }
-      else { print "<span class=bad>FAILED: $q</span><br>\n"; }
-      $q2 = "UPDATE `innreach_stats_by_ptype` SET `have`='$status' WHERE `call` = '$realcall'";
-      if (mysql_query($q2)) { print "<span class=good>DONE: $q2</span><br>\n"; }
-      else { print "<span class=bad>FAILED: $q</span><br>\n"; }
+      $q = "UPDATE `innreach_titles_by_call` SET `have`=? WHERE `call` = ?";
+      $params = array($status,$realcall);
+      $stmt = $db->prepare($q);
+      $ok = $stmt->execute($params);
+      $params_print = print_r($params, true);
+      if ($ok) { print "<span class=good>DONE: $q;$params_print</span><br>\n"; }
+      else { print "<span class=bad>FAILED: $q;$params_print</span><br>\n"; }
 
+      $q2 = "UPDATE `innreach_stats_by_ptype` SET `have`=? WHERE `call` = ?";
+      $stmt = $db->prepare($q2);
+      $ok = $stmt->execute($params);      
+      if ($ok) { print "<span class=good>DONE: $q2; $params_print</span><br>\n"; }
+      else { 
+          print "<span class=bad>FAILED: $q; $params_print</span><br>\n"; 
+      }
     } // end if new info
   } //end foreach submission
 } // end if submission
 
 $q = "SELECT `call`,title FROM `innreach_stats_by_ptype` WHERE ((`have` is null) or (`have` = '')) and ((`title` is not null) and (`title` != '')) and `ptype` = 'all'"; 
 
-$r = mysql_query ($q);
+$stmt = $db->query($q);
 
-print "<form action=\"$PHP_SELF\" method=POST>\n";
+print '<form action="'.$_SERVER['PHP_SELF'].'" method=POST>'.PHP_EOL;
 print "<table>\n";
 print "<tr><th>Call #</th> <th>Title</th> <th>Have? (Y/N/date of alt ed./ebook/etc)</th></tr>\n";
-while ($myrow = mysql_fetch_assoc($r)) {
+while ($myrow = $stmt->fetch(PDO::FETCH_ASSOC)) {
   extract($myrow);
   if (preg_match("/(.*)(\/)/", $title,$m)) 
     $link_title = $m[1];
