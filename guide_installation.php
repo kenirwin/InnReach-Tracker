b2e1da550f9212db6b93610aae97604b5979d54d
Ken Irwin <kirwin@wittenberg.edu>
2019-05-09 15:21:37 -0400

    updated guide_install with PDO

--- guide_installation.php
+++ guide_installation.php
@@ -1,13 +1,13 @@
 <?php
+
 function GuideInstallation ($verbose = false) {
   global $debug;
   if (! $debug) { ERROR_REPORTING(0); }
   include ("config.php");
+  include ("pdo_connect.php");
   $print = "<h1>InnReach Tracker - Installation Progress</h1>\n";
   $config_vars = array ("Local_Institution_Name", "Local_Institution_Short_Name", "Local_Catalog_Name", "Local_Catalog_URL", "InnReach_Catalog_URL", "InnReach_Catalog_Name", "MySQL_Host", "MySQL_Database", "MySQL_User", "MySQL_Password");
-
-  $db = mysql_connect($MySQL_Host,$MySQL_User,$MySQL_Password);
-  if (! mysql_select_db($MySQL_Database,$db)) {
+  if (is_null($db)) {
     $print .= "<h2>First: create a new MySQL database</h2>\n";
     $print .= "<p>Call it whatever you want, ('innreach', or 'innreach_tracker' would be sensible.) Whatever you call it, that'll be the value you give to the MySQL_Database variable in config.php.</p>\n";
   } //end if not connected to a mysql database
@@ -30,21 +30,22 @@ function GuideInstallation ($verbose = false) {
     $print .= "<li class=bad>Please add values to ptypes array in config.php</li>\n"; 
     $incomplete = true;
   } //end else
-  $print .= "</ul>\n";
+$print .= "</ul>\n";
 
-  $print .= "<h2>Connect to MySQL Database</h2>\n";
-  /* only try to connect to mysql database if we're ok so far */
-  if (! $incomplete) {
-    $print .= "<ul>\n";
-    $db = mysql_connect($MySQL_Host,$MySQL_User,$MySQL_Password);
-    if (mysql_select_db($MySQL_Database,$db))
-      $print .= "<li class=good>Connected to MySQL Database $MySQL_Database</li>\n";
-    else {
-      $print .= "<li class=bad>Unable to connect to MySQL Database $MySQL_Database with variables assigned in config.php</li>\n"; 
-      $incomplete = true;
-    } //end else if unable to connect to mysql
-    $print .= "</ul>\n";
-  } //end if trying to connect to mysql / config variables are assigned
+$print .= "<h2>Connect to MySQL Database</h2>\n";
+/* only try to connect to mysql database if we're ok so far */
+if (! $incomplete) {
+$print .= "<ul>\n";
+if (get_class($db) == 'PDO') {
+$print .= "<li class=good>Connected to MySQL Database $MySQL_Database</li>\n";
+}
+else {
+$print .= "<li class=bad>Unable to connect to MySQL Database $MySQL_Database with variables assigned in config.php</li>\n"; 
+$incomplete = true;
+}
+$print .= "</ul>\n";
+
+} //end if trying to connect to mysql / config variables are assigned
   
   else { $print .= "<p class=bad>We'll try that once your variable are established in config.php</p>"; }
   
@@ -52,9 +53,9 @@ function GuideInstallation ($verbose = false) {
   if (! $incomplete) {
     $print .= "<ul>\n";
     $required_tables = array("innreach_by_call", "innreach_by_title", "innreach_stats_by_ptype", "innreach_titles_by_call", "major_lc");
-    $r = mysql_query ("SHOW TABLES");
+    $stmt = $db->query("SHOW TABLES");
     $extant_tables = array();
-    while ($myrow = mysql_fetch_row($r)) {
+while ($myrow = $stmt->fetch(PDO::FETCH_NUM)) {
       array_push($extant_tables, $myrow[0]);
     }
     foreach ($required_tables as $table) {
@@ -69,14 +70,14 @@ function GuideInstallation ($verbose = false) {
   } //end if all connected to mysql
 
   else { $print .= "<p class=bad>We'll try that once you're able to connect to MySQL</p>\n"; }
-  
+
   if (! $incomplete) {
     $print .= "<ul>\n";
     $tables_with_preloaded_data = array ("major_lc", "innreach_titles_by_call");
     foreach ($tables_with_preloaded_data as $table) {
       $q = "SELECT count(*) FROM $table";
-      $r = mysql_query($q);
-      $n = (mysql_fetch_row($r));
+$stmt = $db->query($q);
+$n = $stmt->fetch(PDO::FETCH_NUM);
       if ($n[0] > 200)
 	$print .= "<li class=good>Table $table contains data</li>\n";
       else {
@@ -86,17 +87,19 @@ function GuideInstallation ($verbose = false) {
     } //end while checking for data in tables
     $print .= "</ul>\n";
   } //end if no errors creating tables
+
+
   
   $print .= "<h2>Load InnReach data from your III catalog</h2>\n";
   if (! $incomplete) {
-    /* overwrite all the precding info and move on to more important stuff */
+    /* overwrite all the preceding info and move on to more important stuff */
     $print = "<h1 class=good>The InnreachTracker is successfully installed!</h1><p>Next we need to get data from your catalog into the database.</p>";
     $print .= "<ul>\n";
     $tables_with_user_loaded_data = array ("innreach_by_call", "innreach_by_title", "innreach_stats_by_ptype");
     foreach ($tables_with_user_loaded_data as $table) {
       $q = "SELECT count(*) FROM $table";
-      $r = mysql_query($q);
-      $n = (mysql_fetch_row($r));
+$stmt = $db->query($q);
+$n = $stmt->fetch(PDO::FETCH_NUM);
       if ($n[0] > 1)
 	$print .= "<li class=good>Table $table contains data</li>\n";
       else {
@@ -127,6 +130,7 @@ function GuideInstallation ($verbose = false) {
     return false;
   else 
     return true;
+
 } //end function GuideInstallation
 
 
