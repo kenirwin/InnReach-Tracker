8eb0473fef8e3870247bcb7182e0a950e986d3a1
Ken Irwin <kirwin@wittenberg.edu>
2019-05-09 17:52:43 -0400

    only pdo connect once

--- pdo_connect.php
+++ pdo_connect.php
@@ -1,16 +1,23 @@
 <?php
 
+if (! function_exists('ConnectPDO')) {
+    function ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password,$pdo_params) { 
+        $db = new PDO('mysql:host='.$MySQL_Host.';dbname='.$MySQL_Database.';charset=utf8mb4', $MySQL_User, $MySQL_Password);
+        return $db;
+    }
+        }
+
+
+
 try { 
     $pdo_params = array(
         PDO::MYSQL_ATTR_LOCAL_INFILE => true,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     );
-    $db = ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password,$pdo_params);
+    if (! isset($db)) {
+        $db = ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password,$pdo_params);
+    }
 } catch (PDOException $exception) {
-    error_log($exception->getMessage());
+    print ($exception->getMessage());
 }
 
-function ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password,$pdo_params) { 
-    $db = new PDO('mysql:host='.$MySQL_Host.';dbname='.$MySQL_Database.';charset=utf8mb4', $MySQL_User, $MySQL_Password);
-    return $db;
-}
