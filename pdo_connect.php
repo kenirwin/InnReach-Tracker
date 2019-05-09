7406ed989765a66e4ce66927d5161d681f033122
Ken Irwin <kirwin@wittenberg.edu>
2019-05-09 13:03:24 -0400

    add PDO attributes to pdo_connect

--- pdo_connect.php
+++ pdo_connect.php
@@ -1,12 +1,16 @@
 <?php
 
 try { 
-    $db = ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password);
+    $pdo_params = array(
+        PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+    $db = ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password,$pdo_params);
 } catch (PDOException $exception) {
     error_log($exception->getMessage());
 }
 
-function ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password) { 
+function ConnectPDO($MySQL_Host,$MySQL_Database,$MySQL_User,$MySQL_Password,$pdo_params) { 
     $db = new PDO('mysql:host='.$MySQL_Host.';dbname='.$MySQL_Database.';charset=utf8mb4', $MySQL_User, $MySQL_Password);
     return $db;
 }
